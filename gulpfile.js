39493ce70a385da731b3adb9c8ad609d7c918ec6
John Ryan Camatog <john.ryan.camatog@gmail.com>
2016-10-18 16:34:12 +0800

    Add all Material Design icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,74 @@
 // --------------------------------------------------------------------------------
-// Plugins
+// Plugins and Variables
 // --------------------------------------------------------------------------------
 
-var gulp        = require('gulp'),
-    replaceName = require('gulp-replace-name'),
-    svgNg       = require('gulp-svg-ngmaterial');
+var gulp   = require('gulp'),
+    rename = require('gulp-rename'),
+    svgNg  = require('gulp-svg-ngmaterial');
+
+var iconSets = [
+    'action',
+    'alert',
+    'av',
+    'communication',
+    'content',
+    'device',
+    'editor',
+    'file',
+    'hardware',
+    'image',
+    'maps',
+    'navigation',
+    'notification',
+    'places',
+    'social',
+    'toggle'
+];
 
 // --------------------------------------------------------------------------------
-// Material Design Icons (https://github.com/google/material-design-icons) Tasks
+// Material Design Icons Tasks
 // --------------------------------------------------------------------------------
 
-gulp.task('action', function() {
-    return gulp.src('bower_components/material-design-icons/action/svg/production/*24px.svg')
-        .pipe(replaceName(/ic_([0-9A-Za-z_]+)_24px/g, '$1'))
-        .pipe(svgNg({ filename : 'action.svg' }))
-        .pipe(gulp.dest('24px'))
+iconSets.forEach(function(iconSet) {
+    gulp.task(iconSet, function() {
+        return gulp.src('bower_components/material-design-icons/' + iconSet + '/svg/production/*18px.svg')
+            .pipe(rename(function(path) {
+                path.basename = path.basename.replace(/ic_([0-9A-Za-z_]+)_18px/g, '$1');
+                path.extname = '.svg';
+            }))
+            .pipe(svgNg({ filename : iconSet + '-icons.svg' }))
+            .pipe(gulp.dest('18px'))
 
-        &&
+            &&
 
-        gulp.src('bower_components/material-design-icons/action/svg/production/*48px.svg')
-        .pipe(replaceName(/ic_([0-9A-Za-z_]+)_48px/g, '$1'))
-        .pipe(svgNg({ filename : 'action.svg' }))
-        .pipe(gulp.dest('48px'));
+            gulp.src('bower_components/material-design-icons/' + iconSet + '/svg/production/*24px.svg')
+            .pipe(rename(function(path) {
+                path.basename = path.basename.replace(/ic_([0-9A-Za-z_]+)_(26x)*24px/g, '$1');
+                path.extname = '.svg';
+            }))
+            .pipe(svgNg({ filename : iconSet + '-icons.svg' }))
+            .pipe(gulp.dest('24px'))
+
+            &&
+
+            gulp.src('bower_components/material-design-icons/' + iconSet + '/svg/production/*36px.svg')
+            .pipe(rename(function(path) {
+                path.basename = path.basename.replace(/ic_([0-9A-Za-z_]+)_36px/g, '$1');
+                path.extname = '.svg';
+            }))
+            .pipe(svgNg({ filename : iconSet + '-icons.svg' }))
+            .pipe(gulp.dest('36px'))
+
+            &&
+
+            gulp.src('bower_components/material-design-icons/' + iconSet + '/svg/production/*48px.svg')
+            .pipe(rename(function(path) {
+                path.basename = path.basename.replace(/ic_([0-9A-Za-z_]+)_48px/g, '$1');
+                path.extname = '.svg';
+            }))
+            .pipe(svgNg({ filename : iconSet + '-icons.svg' }))
+            .pipe(gulp.dest('48px'));
+    });
 });
 
 // --------------------------------------------------------------------
@@ -29,5 +76,7 @@ gulp.task('action', function() {
 // --------------------------------------------------------------------
 
 gulp.task('default', function() {
-    
+    iconSets.forEach(function(iconSet) {
+        gulp.start(iconSet);
+    });
 });
